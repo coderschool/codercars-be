c25d3283854880849b49eb87370df32ca6745f6a
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-12-22 18:27:51 +0700

    add starter code and prepare data file to import

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,40 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+  } catch (err) {
+    // YOUR CODE HERE
+  }
 };
 
+// TODO: implementing
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+    res.send("implementing...");
+  } catch (err) {
+    // YOUR CODE HERE
+    next(err);
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+  } catch (err) {
+    // YOUR CODE HERE
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+  } catch (err) {
+    // YOUR CODE HERE
+  }
 };
 
 module.exports = carController;
