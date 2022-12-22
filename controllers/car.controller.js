6908374eb8011ed43aa456dc8d0c5c8ee428475d
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-12-22 19:25:08 +0700

    add feature allow to get all cars list

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -13,11 +13,36 @@ carController.createCar = async (req, res, next) => {
 // TODO: implementing
 carController.getCars = async (req, res, next) => {
   try {
-    // YOUR CODE HERE
-    res.send("implementing...");
-  } catch (err) {
-    // YOUR CODE HERE
-    next(err);
+    let { page, limit } = req.query;
+
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 10;
+
+    // number of items skip for selection
+    let offset = limit * (page - 1);
+
+    // query to get total pages based on total cars returned
+    let totalPages = await Car.find({}).sort({ _id: -1 }).count();
+    totalPages = Math.ceil(totalPages / limit);
+
+    // query to get list of cars based on offset and limit
+    const listOfCars = await Car.find({})
+      .sort({ _id: -1 })
+      .skip(offset)
+      .limit(limit);
+
+    // create response data object
+    const responseData = {
+      message: "Get Car List Successfully!",
+      cars: listOfCars,
+      page: page,
+      total: totalPages,
+    };
+
+    // send response
+    res.status(200).send({ data: responseData });
+  } catch (error) {
+    next(error);
   }
 };
 
